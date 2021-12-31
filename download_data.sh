e9e527202e3f5589183c4acaaf49e38426d46f6a
yuhueilee <ylee0026@student.monash.edu>
2021-12-31 12:30:45 +0800

    Update download_data.sh script

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
+## Make directory
+mkdir data
+
 ## Download images
 wget https://storage.googleapis.com/ads-dataset/subfolder-0.zip
 wget https://storage.googleapis.com/ads-dataset/subfolder-1.zip
@@ -14,21 +17,21 @@ wget https://storage.googleapis.com/ads-dataset/subfolder-9.zip
 wget https://storage.googleapis.com/ads-dataset/subfolder-10.zip
 
 ## Unzip folders
-unzip subfolder-0.zip -d /data
-unzip subfolder-1.zip -d /data
-unzip subfolder-2.zip -d /data
-unzip subfolder-3.zip -d /data
-unzip subfolder-4.zip -d /data
-unzip subfolder-5.zip -d /data
-unzip subfolder-6.zip -d /data
-unzip subfolder-7.zip -d /data
-unzip subfolder-8.zip -d /data
-unzip subfolder-9.zip -d /data
-unzip subfolder-10.zip -d /data
-unzip annotations_images.zip -d /data
+unzip subfolder-0.zip -d data/
+unzip subfolder-1.zip -d data/
+unzip subfolder-2.zip -d data/
+unzip subfolder-3.zip -d data/
+unzip subfolder-4.zip -d data/
+unzip subfolder-5.zip -d data/
+unzip subfolder-6.zip -d data/
+unzip subfolder-7.zip -d data/
+unzip subfolder-8.zip -d data/
+unzip subfolder-9.zip -d data/
+unzip subfolder-10.zip -d data/
+unzip annotations_images.zip -d data/
 
 ## Rename annotation folder
-mv /data/image /data/annotations
+mv data/image data/annotations
 
 ## Remove all zip files
 rm subfolder-0.zip
