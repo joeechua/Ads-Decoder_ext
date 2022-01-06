bbbd5de59e8d5999ed8e3e8045171b3873bddb2a
yuhueilee <ylee0026@student.monash.edu>
2022-01-06 15:51:36 +0800

    Rewrite test cases

--- testsuite/test_labels.py
+++ testsuite/test_labels.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import mock_open, patch
 import os
 import sys
 sys.path.append('../')
@@ -9,39 +9,47 @@ class LabelsTest(unittest.TestCase):
     """
     Test Suite for labels.
     """
-    def test_get_symbol_cluster_name(self):
-        with patch.object(labels, 'get_symbol_cluster_name') as mock_get_method:
-            # test the input argument is called once
-            filename = 'cluster_json_file'
-            mock_get_method(filename)
-            mock_get_method.assert_called_once_with(filename)
-            # test the return value has the correct length
-            mock_get_method.return_value = ['cluster_one', 'cluster_two', 'cluster_three']
-            cluster_names = mock_get_method(filename)
-            self.assertEqual(len(cluster_names), 3)
+    @patch('preprocess.labels.json')
+    def test_get_symbol_cluster_name(self, mocked_json):
+        # test the json is called to load the file
+        filename = '../preprocess/clustered_symbol_list.json'
+        labels.get_symbol_cluster_name(filename)
+        mocked_json.loads.assert_called_once()
     
-    def test_load_symbol_cluster(self):
-        with patch.object(labels, 'load_symbol_cluster') as mock_load_method:
-            # test the input argument is called once
-            filename = 'cluster_json_file'
-            mock_load_method(filename)
-            mock_load_method.assert_called_once_with(filename)
-            # test the return values are dictionaries
-            mock_load_method.return_value = ({'cluster_one': 1}, {1: 'cluster_one'})
-            word_to_id, id_to_word = mock_load_method(filename)
-            self.assertEqual(word_to_id['cluster_one'], 1)
-            self.assertEqual(id_to_word[1], 'cluster_one')
+    @patch('preprocess.labels.json')
+    def test_load_symbol_cluster(self, mocked_json):
+        # test the json is called to load the file
+        filename = '../preprocess/clustered_symbol_list.json'
+        word_to_id, id_to_symbol = labels.load_symbol_cluster(filename)
+        mocked_json.loads.assert_called_once()
+        # test the return values are functioning
+        for k, v in word_to_id.items():
+            self.assertEqual(id_to_symbol[v], k)
+        for k, v in id_to_symbol.items():
+            self.assertEqual(word_to_id[v], k)
 
-    def test_load_symbols_annotation(self):
-        with patch.object(labels, 'load_symbols_annotation') as mock_load_method:
-            # test the input argument is called once
-            filename = 'symbols_json_file'
-            mock_load_method(filename)
-            mock_load_method.assert_called_once_with(filename)
+    @patch('preprocess.labels.json')
+    def test_load_symbols_annotation(self, mocked_json):
+        # test the json is called to load the file
+        filename = '../data/annotations/Symbols.json'
+        labels.load_symbols_annotation(filename)
+        mocked_json.load.assert_called_once()
 
-    def test_build_label_encoder(self):
-        # test the label encoder file has created
-        self.assertTrue(os.path.exists(os.path.join('..', labels.le_path)))
+    @patch('preprocess.labels.load_symbols_annotation')
+    def test_build_label_encoder(self, mocked_load_method):
+        # test the load method is called
+        le_path = os.path.join('..', labels.le_path)
+        filename = '../data/annotations/Symbols.json'
+        labels.build_label_encoder(filename, le_path)
+        mocked_load_method.assert_called_once_with(filename)
+        # test the file is written
+        m = mock_open()
+        with patch('builtins.open', m) as mocked_open, patch('pickle.dumps') as mocked_dumps:
+            labels.build_label_encoder(filename, le_path)
+            m.assert_called_with(le_path, 'wb')
+            file = mocked_open()
+            file.write.assert_called_once()
+            mocked_dumps.assert_called_once()
 
 
 if __name__ == "__main__":
