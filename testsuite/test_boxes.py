590e768715b5cf41eaed4a39f7f4ea8ac9d6bc27
yuhueilee <ylee0026@student.monash.edu>
2022-01-07 11:23:32 +0800

    Write test case for preprocess function

--- testsuite/test_boxes.py
+++ testsuite/test_boxes.py
@@ -8,12 +8,36 @@ class BoxesTest(unittest.TestCase):
     """
     Test Suite for boxes.
     """
-    def test_load_symbols_annotation(self):
-        with patch.object(boxes, 'load_symbols_annotation') as mock_load_method:
-            # test the input argument is called once
-            filename = 'symbols_json_file'
-            mock_load_method(filename)
-            mock_load_method.assert_called_once_with(filename)
+    def test_preprocess_boxes(self):
+        with patch('preprocess.boxes.load_symbols_annotation') as mocked_load_annot_method, \
+                patch('preprocess.boxes.write_dict_to_json') as mocked_write_method:
+            annot_fname = '../data/annotations/Symbols.json'
+            mocked_load_annot_method.return_value = {
+                'img1': [[1, 1, 1, 2, 'nature']],   # case 1: xmin equals xmax
+                'img2': [[6, 2, 3, 3, 'happy/exciting']],   # case 2: xmin greater than xmax
+                'img3': [[4, 4, 5, 4, 'sad'], [1, 1, 2, 2, 'angry']],   # case 3: ymin equals ymax, case 4: all coordinates within boundary
+                'img4': [[4, 8, 5, 5, 'nature/natural']]   # case 5: ymin greater than ymax
+            }
+            boxes.preprocess_boxes(annot_fname)
+            # test the methods are called with the corresponding file name
+            mocked_load_annot_method.assert_called_once_with(annot_fname)
+            # check the symbols annotation are preprocessed
+            final_symbols = {
+                'img2': [[3, 2, 6, 3, 'happy/exciting']],
+                'img3': [[1, 1, 2, 2, 'angry']],
+                'img4': [[4, 5, 5, 8, 'nature/natural']]
+            }
+            mocked_write_method.assert_called_once_with(annot_fname, final_symbols)
+            # tes the boundary values
+            mocked_load_annot_method.return_value = {
+                'img1': [[-1, 1, 502, 2, 'nature']],
+                'img2': [[0, 2, 4, 501, 'happy/exciting']]
+            }
+            boxes.preprocess_boxes(annot_fname)
+            final_symbols = {
+                'img2': [[0, 2, 4, 501, 'happy/exciting']]
+            }
+            mocked_write_method.assert_called_with(annot_fname, final_symbols)
 
 
 if __name__ == "__main__":
