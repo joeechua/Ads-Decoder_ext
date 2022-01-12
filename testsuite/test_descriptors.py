a2194a29f8ea3f3bf31b67197672e30fc0f82561
yuhueilee <ylee0026@student.monash.edu>
2022-01-12 20:38:17 +0800

    Add additiona case to test transform function

--- testsuite/test_descriptors.py
+++ testsuite/test_descriptors.py
@@ -55,6 +55,9 @@ class DescriptorsTest(unittest.TestCase):
         vec = t.transform(lst)
         np_array = np.array(t.text_embed_model.get_vector_rep(t.id_to_word[28]))
         torch.testing.assert_close(vec, torch.Tensor(np_array))
+        # case 4: no id but text
+        lst = ["nature", "wild life", "zoo"]
+        vec = t.transform(lst)
 
     def test_strategies_transform(self):
         s = descriptors.StrategiesPreProcessor(root='../data/annotations')
@@ -73,6 +76,9 @@ class DescriptorsTest(unittest.TestCase):
         vec = s.transform(lst)
         np_array = np.array(s.text_embed_model.get_vector_rep(s.id_to_word[5]))
         torch.testing.assert_close(vec, torch.Tensor(np_array))
+        # case 4: no id but text
+        lst = ["scare tactics", "written message", "scary"]
+        vec = s.transform(lst)
 
 
 if __name__ == "__main__":
