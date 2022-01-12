1a2e8157f04cb456c3c93a64d7bc99269ca57012
yuhueilee <ylee0026@student.monash.edu>
2022-01-12 20:07:52 +0800

    Rename symbol

--- testsuite/test_descriptors.py
+++ testsuite/test_descriptors.py
@@ -15,9 +15,9 @@ class DescriptorsTest(unittest.TestCase):
     @patch('preprocess.descriptors.api')
     def test_api(self, mock_api):
         # test api called once when model exists
-        s = descriptors.TextEmbedModel()
-        if not os.path.exists(s.embed_model_name + '.model'):
-            mock_api.load.assert_called_once_with(s.embed_model)
+        t = descriptors.TextEmbedModel()
+        if not os.path.exists(t.embed_model_name + '.model'):
+            mock_api.load.assert_called_once_with(t.embed_model)
 
     def test_sentiments_transform(self):
         s = descriptors.SentimentPreProcessor(root="../data/annotations")
